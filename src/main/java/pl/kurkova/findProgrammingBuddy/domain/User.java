85b7ea1a6d7d533e0a48c51ba96737a3c1cbeeae
kurkova <kurkiewiczpaula@gmail.com>
2019-11-20 17:45:27 +0100

    Create user class

--- src/main/java/pl/kurkova/findProgrammingBuddy/domain/User.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/domain/User.java
@@ -3,18 +3,28 @@ package pl.kurkova.findProgrammingBuddy.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
+@Entity
+@Table (name = "users")
 public class User {
 
-
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-    private String localization;
-    private String level;
+
+    @Column(name = "userName")
+    private String userName;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "levelOfProgramming")
+    private String levelOfProgramming;
 
 }
