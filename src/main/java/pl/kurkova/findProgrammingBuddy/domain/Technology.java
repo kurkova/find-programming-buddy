4d87d65b16c748d9ddebfcb7b148e8838be32442
kurkova <kurkiewiczpaula@gmail.com>
2019-11-21 16:28:42 +0100

    Create Technology class

--- src/main/java/pl/kurkova/findProgrammingBuddy/domain/Technology.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/domain/Technology.java
@@ -1,4 +1,27 @@
 package pl.kurkova.findProgrammingBuddy.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity(name = "technologies")
 public class Technology {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "kindOfTechnology")
+    @Enumerated(EnumType.STRING)
+    private KindOfTechnology kindOfTechnology;
 }
