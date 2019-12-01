c54ebf558f99038dbc05535060657a9ba9a6ee87
kurkova <kurkiewiczpaula@gmail.com>
2019-12-01 16:46:41 +0100

    Create UserDto class

--- src/main/java/pl/kurkova/findProgrammingBuddy/dto/UserDto.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/dto/UserDto.java
@@ -3,6 +3,9 @@ package pl.kurkova.findProgrammingBuddy.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import pl.kurkova.findProgrammingBuddy.domain.Technology;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -12,4 +15,7 @@ public class UserDto {
     private String userName;
     private String city;
     private String levelOfProgramming;
+    private int sizeOfTeam;
+    private List<Technology> technologiesKnown;
+    private List<Technology> technologiesToStudy;
 }
