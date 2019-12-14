460cb90a1a1f17e6d22df92f21d4e9a72264d8c3
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 21:45:07 +0100

    Create src/main/java/pl/kurkova/findProgrammingBuddy/domain/Technology.java

--- src/main/java/pl/kurkova/findProgrammingBuddy/domain/Technology.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/domain/Technology.java
@@ -3,6 +3,7 @@ package pl.kurkova.findProgrammingBuddy.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity(name = "technologies")
 public class Technology {
 
