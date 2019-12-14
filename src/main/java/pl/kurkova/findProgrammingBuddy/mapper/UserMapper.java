48a5af50329114399450faa31f71d606325c6835
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 21:43:50 +0100

    Create UserMapper

--- src/main/java/pl/kurkova/findProgrammingBuddy/mapper/UserMapper.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/mapper/UserMapper.java
@@ -1,4 +1,40 @@
 package pl.kurkova.findProgrammingBuddy.mapper;
 
+import org.springframework.stereotype.Component;
+import pl.kurkova.findProgrammingBuddy.domain.User;
+import pl.kurkova.findProgrammingBuddy.dto.UserDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class UserMapper {
+
+    public User mapToUser (final UserDto userDto){
+        return new User(
+                userDto.getId(),
+                userDto.getUserName(),
+                userDto.getCity(),
+                userDto.getLevelOfProgramming(),
+                userDto.getSizeOfTeam(),
+                userDto.getTechnologiesToStudy()
+        );
+    }
+
+    public UserDto mapToUserDto(final User user){
+        return new UserDto(
+                user.getId(),
+                user.getUserName(),
+                user.getCity(),
+                user.getLevelOfProgramming(),
+                user.getSizeOfTeam(),
+                user.getTechnologiesToStudy()
+        );
+    }
+
+    public List<UserDto> mapToUsersListDto(final List<User> usersList){
+        return usersList.stream()
+                .map(t-> new UserDto (t.getId(), t.getUserName(), t.getCity(), t.getLevelOfProgramming(), t.getSizeOfTeam(),t.getTechnologiesToStudy()))
+                .collect(Collectors.toList());
+    }
 }
