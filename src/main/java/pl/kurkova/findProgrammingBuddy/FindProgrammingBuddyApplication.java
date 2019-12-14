5a9d2cc896f1e4685acbd9b984753c7b69432a25
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 21:51:14 +0100

    Update

--- src/main/java/pl/kurkova/findProgrammingBuddy/FindProgrammingBuddyApplication.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/FindProgrammingBuddyApplication.java
@@ -2,6 +2,7 @@ package pl.kurkova.findProgrammingBuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class FindProgrammingBuddyApplication {
@@ -9,5 +10,7 @@ public class FindProgrammingBuddyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FindProgrammingBuddyApplication.class, args);
 	}
+
+
 
 }
