ae40f2f68588ea59195e8283366af323fe399c1d
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 15:49:40 +0100

    Create DbService

--- src/main/java/pl/kurkova/findProgrammingBuddy/service/DbService.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/service/DbService.java
@@ -1,4 +1,34 @@
 package pl.kurkova.findProgrammingBuddy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.kurkova.findProgrammingBuddy.domain.User;
+import pl.kurkova.findProgrammingBuddy.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Service
 public class DbService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(final User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
 }
