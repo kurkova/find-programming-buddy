aade93bee731a70bd6c6e7349244de4e3280ef35
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 21:46:12 +0100

    Create TechnologyMapper

--- src/main/java/pl/kurkova/findProgrammingBuddy/mapper/TechnologyMapper.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/mapper/TechnologyMapper.java
@@ -1,4 +1,28 @@
 package pl.kurkova.findProgrammingBuddy.mapper;
 
+import org.springframework.stereotype.Component;
+import pl.kurkova.findProgrammingBuddy.domain.Technology;
+import pl.kurkova.findProgrammingBuddy.dto.TechnologyDto;
+
+@Component
 public class TechnologyMapper {
+
+    public Technology mapToTechnology(final TechnologyDto technologyDto){
+        return new Technology(
+                technologyDto.getId(),
+                technologyDto.getName(),
+                technologyDto.getKindOfTechnology(),
+                technologyDto.getUser()
+        );
+    }
+
+    public TechnologyDto mapToTechnologyDto (final Technology technology){
+        return new TechnologyDto(
+                technology.getId(),
+                technology.getName(),
+                technology.getKindOfTechnology(),
+                technology.getUser()
+        );
+    }
+
 }
