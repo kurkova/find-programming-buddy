0c6fabe14aea40269b1884656ca5486523eb7f89
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 21:48:14 +0100

    Update TechnologyDto class

--- src/main/java/pl/kurkova/findProgrammingBuddy/dto/TechnologyDto.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/dto/TechnologyDto.java
@@ -3,12 +3,14 @@ package pl.kurkova.findProgrammingBuddy.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import pl.kurkova.findProgrammingBuddy.domain.KindOfTechnology;
 import pl.kurkova.findProgrammingBuddy.domain.User;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class TechnologyDto {
     private Long id;
     private String name;
