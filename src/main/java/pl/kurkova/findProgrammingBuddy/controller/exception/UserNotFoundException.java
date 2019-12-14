8af33bcb5e607299e35149707d747e9aa09500ec
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 21:21:16 +0100

    Create UserNotFoundException

--- src/main/java/pl/kurkova/findProgrammingBuddy/controller/exception/UserNotFoundException.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/controller/exception/UserNotFoundException.java
@@ -1,4 +1,8 @@
 package pl.kurkova.findProgrammingBuddy.controller.exception;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends Exception {
 }
