02ab65b2743501b934fbe935a187b039d3850ec3
kurkova <kurkiewiczpaula@gmail.com>
2019-12-14 21:50:24 +0100

    Create UserRepository

--- src/main/java/pl/kurkova/findProgrammingBuddy/repository/UserRepository.java
+++ src/main/java/pl/kurkova/findProgrammingBuddy/repository/UserRepository.java
@@ -1,4 +1,22 @@
 package pl.kurkova.findProgrammingBuddy.repository;
 
-public interface UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import pl.kurkova.findProgrammingBuddy.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Override
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    @Override
+    User save(User user);
+
+    @Override
+    void deleteById (Long id);
 }
+
